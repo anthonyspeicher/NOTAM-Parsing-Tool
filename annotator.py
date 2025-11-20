fa2c7c5705a8e6c39f5c64eb75a6e3a6186fd4af
anthonyspeicher <anthony@speicher.org>
2025-11-20 17:43:17 -0500

    Fully fixed annotator, now ignores any NOTAMs that reference others

--- annotator.py
+++ annotator.py
@@ -18,10 +18,13 @@ with open("NOTAM.txt", "w") as file:
         current = df['Unnamed: 6'][i]
 
         # remove whitespace and write
+        string = "".join(current.splitlines())
         if i < len(df['Unnamed: 6']) - 1:
-            file.write("".join(current.splitlines()) + "\n")
-        else:
-            file.write("".join(current.splitlines()))
+            string += "\n"
+        if " SEE LGA " in string:
+            string = ""
+
+        file.write(string)
 
 # parse text file
 with open("NOTAM.txt", "r") as file:
@@ -31,65 +34,71 @@ with open("NOTAM.txt", "r") as file:
     # full list of annotations, each in brackets
     annotations = []
 
-    input = file.read()
-    for line in input.splitlines():
+    input = file.read().splitlines()
+    for i in range(len(input)):
         # current data and labels to be added to annotations
-        #
+        line = input[i]
+
         current = [line]
         # entities list with actual labels
         entities = []
 
         # parse line
         # ACCOUNTABILITY
-        entities.append([1, line.find(" ") - 1, "ACCOUNTABILITY"])
+        entities.append([1,line.find(" "),"ACCOUNTABILITY"])
 
-        # LOCATION_IDENT
+        #LOCATION_IDENT
         count = line.find(" ") + 1
         while line[count] != " ":
             count += 1
         count += 1
 
-        entities.append([count, line.find(" ", count) - 1, "LOCATION_IDENT"])
+        entities.append([count,line.find(" ", count),"LOCATION_IDENT"])
 
+        #NEED HANDLING FOR OBST WIND TURBINE FARM WI AN AREA DEFINED AS 4.5NM RADIUS OF 444703N0711734W
         # OBST_TYPE
         count = line.find(" ", count) + 1
         while line[count] != " ":
             count += 1
         count += 1
 
-        entities.append([count, line.find("(", count) - 2, "OBST_TYPE"])
+        if " WI " in line:
+            entities.append([count,line.find(" WI ", count),"OBST_TYPE"])
+        else:
+            entities.append([count,line.find("(", count) - 1,"OBST_TYPE"])
 
         # LOCATION_EXACT,
-        count = line.find("(", count) + 1
-        while line[count] != ")":
-            count += 1
-        count += 2
+        if " WI " in line:
+            count = line.find(" OF ", count) + 4
+        else:
+            count = line.find(")", count) + 2
 
-        entities.append([count, line.find(" ", count) - 1, "LOCATION_EXACT"])
+        entities.append([count,line.find("(", count) - 1,"LOCATION_EXACT"])
 
         # LOCATION_REL
         count = line.find("(", count) + 1
 
-        entities.append([count, line.find(")", count) - 1, "LOCATION_REL"])
+        entities.append([count,line.find(")", count),"LOCATION_REL"])
         count = line.find(")", count) + 2
 
         # HEIGHT_EXACT
-        entities.append([count, line.find("(", count) - 2, "HEIGHT_EXACT"])
+        entities.append([count,line.find("(", count) - 1,"HEIGHT_EXACT"])
 
         # HEIGHT_REL
         count = line.find("(", count) + 1
 
-        entities.append([count, line.find(")", count) - 1, "HEIGHT_REL"])
+        entities.append([count,line.find(")", count),"HEIGHT_REL"])
         count = line.find(")", count) + 2
 
         # OBST_INFO
         end = line.rfind(" ")
-        entities.append([count, end - 1, "OBST_INFO"])
+        entities.append([count,end,"OBST_INFO"])
 
         # TIME_PERIOD
-        entities.append([end + 1, len(line) - 1, "TIME_PERIOD"])
+        entities.append([end + 1,len(line),"TIME_PERIOD"])
 
-
+        if len(entities) < 9:
+            print(f"Potential issue detected with line {i}")
         current.append({"entities":entities})
         annotations.append(current)
 
